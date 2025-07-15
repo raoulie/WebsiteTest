c1855328ba30ebe91513a8f7fe37477209fc8982
raoulie <61330542+raoulie@users.noreply.github.com>
2025-07-15 10:24:44 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,13 @@
+/*Name this external file gallery.js*/
 
+function upDate(previewPic) {
+    const imageDiv = document.getElementById('image');
+    imageDiv.style.backgroundImage = `url('${previewPic.src}')`;
+    imageDiv.innerHTML = previewPic.alt;
+}
+
+function unDo() {
+    const imageDiv = document.getElementById('image');
+    imageDiv.style.backgroundImage = "url('')";
+    imageDiv.innerHTML = "Hover over an image below to display here.";
+}
