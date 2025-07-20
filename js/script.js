c6a69453ff576219ba97570f1cc671ac655f3fa5
raoulie <61330542+raoulie@users.noreply.github.com>
2025-07-20 16:17:10 +0200

    adapted ripple

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 document.querySelectorAll('.ripple-img').forEach(img => {
   img.addEventListener('pointerdown', e => {
+    const container = img.closest('.ripple-container') || img;
+
     const ripple = document.createElement('span');
     ripple.className = 'ripple';
     ripple.style.left = `${e.offsetX}px`;
     ripple.style.top = `${e.offsetY}px`;
-    img.appendChild(ripple);
+    container.appendChild(ripple);
 
-    setTimeout(() => {
-      ripple.remove();
-    }, 600);
+    setTimeout(() => ripple.remove(), 600);
   });
 });
+
